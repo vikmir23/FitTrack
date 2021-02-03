3078d09ce0059415b7cc85cd73e81861fb587029
Vikram Miryala <vikram.miryala@gmail.com>
2021-02-03 01:28:40 -0800

    minor changes

--- fit_track/lib/main.dart
+++ fit_track/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:fit_test/models/user.dart';
-import 'package:fit_test/screens/wrapper.dart';
-import 'package:fit_test/services/auth.dart';
+import 'package:fit_track/models/user.dart';
+import 'package:fit_track/screens/wrapper.dart';
+import 'package:fit_track/services/auth.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
